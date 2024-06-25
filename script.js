58941d8476100b4c64f932fd0c56eb706768a5b0
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-25 10:22:22 +0100

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,40 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+// calculator functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function calculate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "add":
+            return add(firstNumber, secondNumber);
+            break;
+        case "subtract":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "multiply":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "divide":
+            return divide(firstNumber, secondNumber);
+            break;
+        default:
+            alert("Oops, I don't recognise that operation")
+    }
+
+}
